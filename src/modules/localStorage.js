827bc6a50a42e4d6ecd71f94b943880b2b58b361
Ralph-tech <58685004+Ralph-tech@users.noreply.github.com>
2021-05-19 13:23:47 +0300

    Add local storage

---
+++ src/modules/localStorage.js
@@ -0,0 +1,19 @@
+
+
+// Local storage
+
+ const addToLocalStorage = (listOfProjects) =>{
+  localStorage.setItem('listOfProjects', JSON.stringify(listOfProjects));
+  create();
+}
+
+// Add the selected project to local storage
+ const addSelectedProject = (selectedProjectId) =>{
+  localStorage.setItem('selectedProjectId', JSON.stringify(selectedProjectId));
+  create();
+}
+
+export {
+  addToLocalStorage,
+  addSelectedProject
+}
